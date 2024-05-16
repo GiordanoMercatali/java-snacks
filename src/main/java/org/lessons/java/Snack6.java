de257bf083da8d6523316c0db8fb520273e6a7bc
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-16 17:03:17 +0200

    add: snack 6 can handle numbers with more than 1 digit

--- src/main/java/org/lessons/java/Snack6.java
+++ src/main/java/org/lessons/java/Snack6.java
@@ -2,8 +2,13 @@ package org.lessons.java;
 // import java.util.Scanner;
 public class Snack6 {
     public static void main(String[] args) {
-        String stringNumber = "4";
-        int result = stringNumber.charAt(0) - '0';
-        System.out.println(result);
+        String stringNumber = "415";
+        int result = 0;
+        
+        for (int i = 0; i < stringNumber.length(); i++) {
+            int addNumber = stringNumber.charAt(i) - '0';
+            result = result * 10 + addNumber;
+            
+        }System.out.println(result);
     }
 }
