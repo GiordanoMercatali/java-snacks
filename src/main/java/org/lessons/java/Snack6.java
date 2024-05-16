1fad74255693ccba67f9abbeb3a9d164404674d9
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-16 16:45:41 +0200

    add: snack 6

---
+++ src/main/java/org/lessons/java/Snack6.java
@@ -0,0 +1,9 @@
+package org.lessons.java;
+// import java.util.Scanner;
+public class Snack6 {
+    public static void main(String[] args) {
+        String stringNumber = "4";
+        int result = stringNumber.charAt(0) - '0';
+        System.out.println(result);
+    }
+}
